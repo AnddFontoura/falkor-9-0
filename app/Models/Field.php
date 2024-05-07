94c377c8938e9e6afaa2e281c3d303c7e7bc98ee
kdpenha <kayrodrigues0410@gmail.com>
2024-05-07 14:09:16 -0300

    created Field Model

---
+++ app/Models/Field.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Field extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'city_id',
+        'name',
+        'nickname',
+        'address',
+        'google_location',
+        'owner'
+    ];
+}
