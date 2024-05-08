5949af46cc9ab22db54f4cd3a7fc3119db19cd93
kdpenha <kayrodrigues0410@gmail.com>
2024-05-08 13:40:42 -0300

    added /api/fields route pointing to search method in fieldcontroller. i will improve the code

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,9 @@ Route::prefix('system')->middleware('auth')->name('system.')->group(function() {
             Route::get('create/{matchId}', 'form')->name('form');
             Route::post('update/{matchId}', 'store')->name('update');
         });
-
+    
+    Route::get('/api/fields', [FieldController::class, 'search'])
+        ->name('fields.api');
     Route::resource('fields', FieldController::class);
 });
+
