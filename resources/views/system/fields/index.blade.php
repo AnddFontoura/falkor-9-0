68469a256e49948c1d2901ec337ed6021c7da8f9
kdpenha <kayrodrigues0410@gmail.com>
2024-05-10 11:44:09 -0300

    correcting paginate

--- resources/views/system/fields/index.blade.php
+++ resources/views/system/fields/index.blade.php
@@ -19,16 +19,16 @@
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label for="fieldName">Nome do campo</label>
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Nome do campo" value="{{ Request::get('teamName') ?? '' }}">
+                                <input type="text" class="form-control" id="fieldName" name="fieldName" placeholder="Nome do campo" value="{{ Request::get('fieldName') ?? '' }}">
                             </div>
                         </div>
 
                         <div class="col-md-4">
                             <label for="fieldState">Estado do campo</label>
                             <select class="form-control select2bs4" id="fieldState" name="state_id">
-                                <option value='' selected>--Selecione um estado--</option>
+                                <option value=''>--Selecione um estado--</option>
                                 @foreach ($states as $state)
-                                    <option value='{{$state->id}}'>{{$state->name}}</option>
+                                    <option value='{{$state->id}}' {{Request::get('state_id') == $state->id ? 'selected' : ''}}>{{$state->name}}</option>
                                 @endforeach
                                 
                             </select>
@@ -37,9 +37,9 @@
                         <div class="col-md-4">
                             <label for="fieldCity">Cidade do campo</label>
                             <select class="form-control select2bs4" id="fieldCity" name="city_id">
-                                <option value='' selected>--Selecione uma cidade--</option>
+                                <option value=''>--Selecione uma cidade--</option>
                                 @foreach ($cities as $city)
-                                    <option value='{{$city->id}}'>{{$city->name}}</option>
+                                    <option value='{{$city->id}}' {{Request::get('city_id') == $city->id ? 'selected' : ''}}>{{$city->name}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -59,46 +59,36 @@
     
     @if($fields->count() > 0)
         <div class="col-md-12 d-flex flex-column align-items-center">
-        <table class="table ">
-            <thead class="thead-light">
-                <tr>
-                <th scope="col">ID do Campo</th>
-                <th scope="col">City ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Apelido</th>
-                <th scope="col">Endereco</th>
-                <th scope="col">Como chegar</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($fields as $field)
-                <tr>
-                <td>{{$field->id}}</td>
-                <td>{{$field->cityInfo->name}}</td>                
-                <td>{{$field->name}}</td>
-                <td>{{$field->nickname != ''? $field->nickname : '---'}}</td>
-                <td>{{$field->address}}</td>
-                <td><a href="{{$field->google_location}}">{{$field->google_location}}</a></td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+            <table class="table ">
+                <thead class="thead-light">
+                    <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Apelido</th>
+                    <th scope="col">Endereco</th>
+                    <th scope="col">Como chegar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($fields as $field)
+                    <tr>
+                    <td>{{$field->name}}</td>
+                    <td>{{$field->cityInfo->name}}</td>
+                    <td>{{$field->cityInfo->stateInfo->name}}</td>
+                    <td>{{$field->nickname != ''? $field->nickname : '---'}}</td>
+                    <td>{{$field->address}}</td>
+                    <td><a href="{{$field->google_location}}">{{$field->google_location}}</a></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-            <nav>
-                <ul class="pagination">
-                    <li class="page-item"><a class="page-link" href="{{ $fields->previousPageUrl() }}">Voltar</a></li>
-                    {{--Link de retorno para o paginate--}}
-
-                    @for($i = 1; $i <= $fields->lastPage(); $i++) 
-                    <li class="page-item {{ $fields->currentPage() == $i ? 'active' : ''}}" >
-                        <a class="page-link" href="{{ $fields->url($i) }}">{{ $i }}</a>
-                    </li>
-                    @endfor
-
-                    <li class="page-item"><a class="page-link" href="{{ $fields->nextPageUrl() }}">Proximo</a></li>
-                    {{--Link de avancar para o paginate--}}
-                </ul>
-            </nav>
+            @if($fields->links())
+                <div class="col-12 mt-3">
+                    {{ $fields->withQueryString()->links() }}
+                </div>
+            @endif
         </div>
 </div>
 
