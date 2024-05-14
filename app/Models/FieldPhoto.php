7d66d34b5af434e37e07700ea5f76d03aa4b673f
kdpenha <kayrodrigues0410@gmail.com>
2024-05-14 17:37:53 -0300

    $fillable from FieldPhoto for create method

--- app/Models/FieldPhoto.php
+++ app/Models/FieldPhoto.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class FieldPhoto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'field_id',
+        'main',
+        'photo'
+    ];
 }
