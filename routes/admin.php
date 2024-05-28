4f220cabe34a335d389c4133f17d2db194885c4e
kdpenha <kayrodrigues0410@gmail.com>
2024-05-28 19:37:56 -0300

    Criação das rotas de 'admin' separadas

---
+++ routes/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\MatchHasPlayerController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "admin" middleware group. Enjoy building your Admin!
+|
+*/
+
+Route::prefix('admin')
+        ->controller(AdminController::class)
+        ->name('admin.')
+        ->middleware(['auth','isAdmin',])
+        ->group(function() {
+        Route::get('/', 'index')->name('index');
+        Route::get('show/{userId}', 'show')->name('show');
+    });
