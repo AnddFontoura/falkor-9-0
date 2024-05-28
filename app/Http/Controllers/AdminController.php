ab57d7fc6cc9ac33937afc29059c52961dbf344d
kdpenha <kayrodrigues0410@gmail.com>
2024-05-28 19:41:08 -0300

    Criação do Controller de Admin e implantação dos métodos index e show

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+
+class AdminController extends Controller
+{
+    protected $model;
+
+    public function __construct(User $model) {
+        $this->model = $model;
+        parent::__construct();
+    }
+
+    public function index(): View
+    {
+        $users = $this->model->paginate(20);
+        return view('system.admin.index', compact('users'));
+    }
+
+    public function show(int $id): View
+    {
+        $user = $this->model->where('id', $id)->first();
+        $tempo_cadastrado = $this->dateService->getRegistrationTime($user);
+        return view('system.admin.show', compact('user', 'tempo_cadastrado'));
+    }
+
+    public function edit($id)
+    {
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    
+    public function destroy($id)
+    {
+        //
+    }
+}
