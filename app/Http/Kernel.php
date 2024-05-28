84db32dd5fb0a77f82df516bf877294667bea310
kdpenha <kayrodrigues0410@gmail.com>
2024-05-28 19:42:04 -0300

    Adicionando o middleware de IsAdmin e colocando 'admin' no grupo de rotas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\IsAdminMiddleware;
 use App\Http\Middleware\IsTeamManagerMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -45,6 +46,14 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'admin' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]
     ];
 
     /**
@@ -64,6 +73,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'isTeamManager' => IsTeamManagerMiddleware::class
+        'isTeamManager' => IsTeamManagerMiddleware::class,
+        'isAdmin' => IsAdminMiddleware::class
     ];
 }
