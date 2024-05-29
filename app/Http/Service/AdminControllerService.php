d7d5904530064c246fef64839a6336fc100bb604
kdpenha <kayrodrigues0410@gmail.com>
2024-05-29 15:31:48 -0300

    metodo de search e destroy dentro do service

--- app/Http/Service/AdminControllerService.php
+++ app/Http/Service/AdminControllerService.php
@@ -8,6 +8,60 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminControllerService
 {
+    public function searchUserIndex(Request $request, ?User $user)
+    {
+        
+        $this->validateSearchIndex($request);
+        $filter = request()->only([
+            'userId',
+            'userName',
+            'userEmail',
+            'email_verified_at',
+            'deleted_at',
+            'is_admin',
+        ]);
+
+        $users = $user->select('users.*');
+
+        if(isset($filter['userId']) && $filter['userId'] != null) {
+            $users = $users->where('users.id', '=' , $filter['userId']);
+        }
+
+        if(isset($filter['userName']) && $filter['userName'] != null) {
+            $users = $users->where('users.name', 'like' , '%' . $filter['userName'] . '%');
+        }
+
+        if(isset($filter['userEmail']) && $filter['userEmail'] != null) {
+            $users = $users->where('users.email', 'like' , '%' . $filter['userEmail'] . '%');
+        }
+
+        if(isset($filter['email_verified_at']) && $filter['email_verified_at']) {
+            $users = $users->where('users.email_verified_at', '!=' , null);
+        }
+
+        if(isset($filter['deleted_at']) && $filter['deleted_at']) {
+            $users = $users->where('users.deleted_at', '!=' , null);
+        }
+
+        if(isset($filter['is_admin']) && $filter['is_admin']) {
+            $users = $users->where('users.is_admin', '=' , 1);
+        }
+
+        return $users->withTrashed()->paginate(20);
+    }
+
+    public function validateSearchIndex(Request $request)
+    {
+        $request->validate([
+            'userId' => 'nullable|integer',
+            'userName' => 'nullable|string|min:1|max:254',
+            'userEmail' => 'nullable|max:254',
+            'email_verified_at' => 'nullable|integer',
+            'deleted_at' => 'nullable|integer',
+            'is_admin' => 'nullable|integer'
+        ]);
+    }
+
     public function validateDataFromForm(Request $request, int $id): void
     {
         $user = $this->getUser($id);
@@ -15,7 +69,7 @@ class AdminControllerService
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id],
-            'password' => ['required', 'string', 'min:8'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
@@ -41,8 +95,19 @@ class AdminControllerService
         $user->fill($data);
     }
 
+    public function destroyUser(int $id): void
+    {  
+        $user = $this->getUser($id);
+        $user->delete();
+    }
+
     public function getUser($id): ?User
     {
         return User::where('id', $id)->first();
+    }
+
+    public function getModel(): User
+    {
+        return new User();
     }
 }
