dfec37e4cab5a21c2455652377aac1e78e39144e
kdpenha <kayrodrigues0410@gmail.com>
2024-05-29 19:41:14 -0300

    separando os uses de acordo com os padroes da psr4

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,10 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
