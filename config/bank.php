794ebcb9b8c84958eb3de48e53b9e05402100043
André Fontoura <anddfontoura91@gmail.com>
2024-05-29 13:09:40 -0300

    Começando os detalhes da integração do Inter banking

--- config/bank.php
+++ config/bank.php
@@ -5,7 +5,7 @@ return [
       'base_url' => 'https://cdpj.partners.bancointer.com.br/',
       'client_id' => env('INTER_CLIENT_ID'),
       'client_secret' => env('INTER_CLIENT_SECRET'),
-      'certificate' => asset('bank_certificate/inter_api_certificado.crt'),
-      'key' => asset('bank_certificate/inter_api_chave.key'),
-  ],
+      'certificate' => 'bank_certificate/inter_api_certificado.crt',
+      'key' => 'bank_certificate/inter_api_chave.key',
+  ]
 ];
