6188572b706a4427d6f583f88bc19c2f62c6c805
kdpenha <kayrodrigues0410@gmail.com>
2024-06-03 13:37:26 -0300

    aplicando hasOne userPlan para o User

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -45,4 +46,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function userPlan(): HasOne
+    {
+        return $this->hasOne(UserPlan::class, 'id');
+    }
 }
