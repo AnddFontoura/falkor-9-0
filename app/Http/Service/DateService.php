493d77dc6b9205f54aaa3ca93d12bc752cb46063
kdpenha <kayrodrigues0410@gmail.com>
2024-06-03 17:35:40 -0300

    novo metodo para o dateservice, ele pega a finish date do plano

--- app/Http/Service/DateService.php
+++ app/Http/Service/DateService.php
@@ -19,4 +19,18 @@ class DateService
             'syntax' => Carbon::DIFF_RELATIVE_TO_NOW // formato relativo ao agora
         ]);
     }
+
+    public function getPlanFinishDate(object $model)
+    {
+        Carbon::setLocale('pt_BR');
+
+        $finishDate = new Carbon($model->finish_date);
+
+        return $planFinishDate = $finishDate->diffForHumans([
+            'parts' => 3, //mostra ate 3 unidades de tempo
+            'join' => true, //junta as unidades com 'e'
+            'short' => false, //usa a forma completa, nao abreviada
+            'syntax' => Carbon::DIFF_RELATIVE_TO_NOW // formato relativo ao agora
+        ]);
+    }
 }
