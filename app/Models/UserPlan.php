6d5e120b95808adeed8c772db3bb18286699d633
kdpenha <kayrodrigues0410@gmail.com>
2024-06-03 17:36:47 -0300

    relacionamento de userPlan para com Plan

--- app/Models/UserPlan.php
+++ app/Models/UserPlan.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserPlan extends Model
 {
@@ -18,4 +19,9 @@ class UserPlan extends Model
         'features',
         'price',
     ];
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
