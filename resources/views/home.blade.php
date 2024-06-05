d1e8b229d5730ce6728c2d26768e9ee3cb5906d1
André Fontoura <anddfontoura91@gmail.com>
2024-06-05 10:12:49 -0300

    Ajustes no PR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,46 +58,48 @@
                 </div>
                 <div class="card-body">
                     <div class="d-flex flex-column flex-md-row">
+                        <div class="row">
                         @foreach($filteredTeamsPlayer as $teamPlayer)
                             @php
-                            isset($teamPlayer->teamInfo->logo_path) ?
-                            $logoPath = asset('storage/' . $teamPlayer->teamInfo->logo_path)
-                            : $logoPath = asset('img/dragon.png');
+                                isset($teamPlayer->teamInfo->logo_path) ?
+                                    $logoPath = asset('storage/' . $teamPlayer->teamInfo->logo_path)
+                                    : $logoPath = asset('img/dragon.png');
                             @endphp
 
-                            <div class="col-md-4">
-                            <div class="card">
-                                <div class="card-header" data-toggle="collapse" data-target="#team-player-{{ $teamPlayer->id }}" style="cursor:pointer;">
-                                    <figure class="d-flex justify-content-center">
-                                        <img style="height:120px" class="img-fluid" src="{{ $logoPath }}">
-                                    </figure>
-                                </div>
-                                <div id="team-player-{{ $teamPlayer->id }}" class="card-body border collapse text-center bg-light">
-                                    <h4 class="text-info"><a href="{{ route('system.team.show', [$teamPlayer->team_id]) }}">{{ $teamPlayer->teamInfo->name }}</a></h4>
-                                    <div>
-                                        <span class="text-bold">Posição</span>: {{ $teamPlayer->gamePositionInfo->name }}
+                            <div class="col-md-4 col-lg-4 col-sm-12 mt-1">
+                                <div class="card">
+                                    <div class="card-header" data-toggle="collapse" data-target="#team-player-{{ $teamPlayer->id }}" style="cursor:pointer;">
+                                        <figure class="d-flex justify-content-center">
+                                            <img style="height:120px" class="img-fluid" src="{{ $logoPath }}">
+                                        </figure>
                                     </div>
-                                    <div>
-                                        <span class="text-bold">Número da camisa</span>: {{ $teamPlayer->number }}
-                                    </div>
-                                    <div>
-                                        <span class="text-bold">Ativo</span>:
-                                            @if($teamPlayer->active)
-                                                <span class="text-success">Sim</span>
-                                            @else
-                                                <span class="text-danger">Não</span>
-                                            @endif
+                                    <div id="team-player-{{ $teamPlayer->id }}" class="card-body border collapse text-center bg-light">
+                                        <h4 class="text-info"><a href="{{ route('system.team.show', [$teamPlayer->team_id]) }}">{{ $teamPlayer->teamInfo->name }}</a></h4>
+                                        <div>
+                                            <span class="text-bold">Posição</span>: {{ $teamPlayer->gamePositionInfo->name }}
+                                        </div>
+                                        <div>
+                                            <span class="text-bold">Número da camisa</span>: {{ $teamPlayer->number }}
+                                        </div>
+                                        <div>
+                                            <span class="text-bold">Ativo</span>:
+                                                @if($teamPlayer->active)
+                                                    <span class="text-success">Sim</span>
+                                                @else
+                                                    <span class="text-danger">Não</span>
+                                                @endif
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                         @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -111,69 +113,71 @@
                 </div>
                 <div class="card-body">
                     <div class="d-flex flex-column flex-md-row">
+                        <div class="row">
                         @foreach($ownedTeams as $ownedTeam)
-                        @php
-                            isset($ownedTeam->logo_path) && $ownedTeam->logo_path != '' ?
-                            $logoPath = asset('storage/' . $ownedTeam->logo_path)
-                            : $logoPath = asset('img/dragon.png');
+                            @php
+                                isset($ownedTeam->logo_path) && $ownedTeam->logo_path != '' ?
+                                $logoPath = asset('storage/' . $ownedTeam->logo_path)
+                                : $logoPath = asset('img/dragon.png');
 
-                            $isPlayerInOwnedTeam = in_array($ownedTeam->id, $teamsPlayingIds);
+                                $isPlayerInOwnedTeam = in_array($ownedTeam->id, $teamsPlayingIds);
 
-                            $playerDetails = null;
+                                $playerDetails = null;
 
-                            foreach($teamsPlayer as $player) {
-                                if($player->team_id == $ownedTeam->id) {
-                                    $playerDetails = $player;
-                                    break;
+                                foreach($teamsPlayer as $player) {
+                                    if($player->team_id == $ownedTeam->id) {
+                                        $playerDetails = $player;
+                                        break;
+                                    }
                                 }
-                            }
-                        @endphp
-                        <div class="col-md-4">
-                            <div class="card">
-                                <div class="card-header" data-toggle="collapse" data-target="#owned-team-{{ $ownedTeam->id }}" style="cursor:pointer;">
-                                    <figure class="d-flex justify-content-center">
-                                        <img style="height:120px" class="img-fluid" src="{{ $logoPath }}">
-                                    </figure>
-                                </div>
-                                <div id="owned-team-{{ $ownedTeam->id }}" class="card-body border collapse text-center bg-light">
-                                    <h4 class="text-info"><a href="{{ route('system.team.show', [$ownedTeam->id]) }}">{{ $ownedTeam->name }}</a></h4>
-                                    <div>
-                                        <span class="text-bold">Posição</span>: Dono
+                            @endphp
+                            <div class="col-md-4 col-lg-4 col-sm-12 mt-1">
+                                <div class="card">
+                                    <div class="card-header" data-toggle="collapse" data-target="#owned-team-{{ $ownedTeam->id }}" style="cursor:pointer;">
+                                        <figure class="d-flex justify-content-center">
+                                            <img style="height:120px" class="img-fluid" src="{{ $logoPath }}">
+                                        </figure>
                                     </div>
-                                    <div>
-                                        <span class="text-bold">Jogador</span>: 
+                                    <div id="owned-team-{{ $ownedTeam->id }}" class="card-body border collapse text-center bg-light">
+                                        <h4 class="text-info"><a href="{{ route('system.team.show', [$ownedTeam->id]) }}">{{ $ownedTeam->name }}</a></h4>
+                                        <div>
+                                            <span class="text-bold">Posição</span>: Dono
+                                        </div>
+                                        <div>
+                                            <span class="text-bold">Jogador</span>:
+                                            @if($isPlayerInOwnedTeam)
+                                                <span class="text-success">Sim</span>
+                                            @else
+                                                <span class="text-danger">Não</span>
+                                            @endif
+                                        </div>
                                         @if($isPlayerInOwnedTeam)
-                                            <span class="text-success">Sim</span>
-                                        @else
-                                            <span class="text-danger">Não</span>
+                                                <div>
+                                                    <span class="text-bold">Posição em campo</span>: {{ $playerDetails->gamePositionInfo->name }}
+                                                </div>
+                                                <div>
+                                                    <span class="text-bold">Número da camisa</span>: {{ $playerDetails->number }}
+                                                </div>
+                                                <div>
+                                                    <span class="text-bold">Ativo</span>:
+                                                        @if($playerDetails->active)
+                                                            <span class="text-success">Sim</span>
+                                                        @else
+                                                            <span class="text-danger">Não</span>
+                                                        @endif
+                                                </div>
                                         @endif
                                     </div>
-                                    @if($isPlayerInOwnedTeam)
-                                            <div>
-                                                <span class="text-bold">Posição em campo</span>: {{ $playerDetails->gamePositionInfo->name }}
-                                            </div>
-                                            <div>
-                                                <span class="text-bold">Número da camisa</span>: {{ $playerDetails->number }}
-                                            </div>
-                                            <div>
-                                                <span class="text-bold">Ativo</span>:
-                                                    @if($playerDetails->active)
-                                                        <span class="text-success">Sim</span>
-                                                    @else
-                                                        <span class="text-danger">Não</span>
-                                                    @endif
-                                            </div>
-                                    @endif
                                 </div>
                             </div>
-                        </div>                        
-                    @endforeach
+                          @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -195,7 +199,7 @@
 
                             $isVisitant = $nextMatch->homeTeamInfo == null;
                             $opposingTeam = $isVisitant ? $nextMatch->home_team_name : $nextMatch->visitor_team_name;
-                            
+
                             $myTeamLogo = isset($myTeam->logo_path) ?
                                 asset('storage/' . $myTeam->logo_path)
                                 : asset('img/dragon.png');
@@ -245,89 +249,8 @@
             </div>
         </div>
     </div>
-    
-</div>
-
-{{--<div class="row">
-
-    @if(count($playerInvitations) > 0)
-    <div class="col-md-4 col-lg-3 col-sm-12 mt-3 p-1">
-        <a href="{{ route('system.player-invitation.index') }}">
-            <div class="alert alert-warning alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h5><i class="icon fas fa-exclamation-triangle"></i> Você tem um convite ativo!</h5>
-                Algum time te convidou para fazer parte do elenco, clique aqui para avaliar o convite.
-            </div>
-        </a>
-    </div>
-    @endif
-</div>
-
-<div class="row">
-        <div class="col-12 d-flex flex-column mt-2">
-                <div class="col-6 d-block">
-                    <h2 class="m-0 p-0">Dashboard</h2>
-                </div>
-                <div class="col-6 d-flex">
-                    <div class="col-4 info-box bg-warning">
-                        <span class="info-box-icon"><i class="fa fa-user-circle" aria-hidden="true"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Plano</span>
-                            <span class="info-box-number">Dente de Leite</span>
-                        </div>
-                    </div>
-                </div>
-        </div>
-</div>
-
-<div class="row">
-        <div class="card">
-            <div class="card-header border-transparent">
-                <h3 class="card-title">Latest Orders</h3>
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                </div>
-            </div>
-
-            <div class="card-body p-0">
-                <div class="table-responsive">
-                    <table class="table m-0">
-                        <thead>
-                            <tr>
-                                <th>Order ID</th>
-                                <th>Item</th>
-                                <th>Status</th>
-                                <th>Popularity</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                                <td>Call of Duty IV</td>
-                                <td><span class="badge badge-success">Shipped</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
-                                </td>
-                            </tr>    
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-            <div class="card-footer clearfix">
-                <a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Order</a>
-                <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All Orders</a>
-            </div>
 
 </div>
 
-</div>
-
-</div> --}}
 
 @endsection
