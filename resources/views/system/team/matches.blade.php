23894d43d8a33e63214c984520d13926535d2c4e
André Fontoura <anddfontoura91@gmail.com>
2024-06-23 20:16:43 -0300

    Modifica um pedaço do comportamento dos matches de times, ideia é simplificar as urls

--- resources/views/system/team/matches.blade.php
+++ resources/views/system/team/matches.blade.php
@@ -36,51 +36,51 @@
         <div class='alert alert-danger'> Nenhuma partida cadastrada </div>
     </div>
     @else
-    @foreach($matches as $match)
-    <div class="col-md-4">
+        @foreach($matches as $match)
+        <div class="col-md-4">
 
-        <div class="card card-widget widget-user">
-            <div class="card-footer">
-                <div class="row">
-                    <div class="col-sm-6 border-right">
-                        <div class="description-block">
-                            <h5 class="description-header"> {{ $match->home_team_name }} </h5>
-                            <span class="description-text"> {{ $match->home_score ?? 'Sem Resultado' }}</span>
-                        </div>
-
-                    </div>
-
-                    <div class="col-sm-6">
-                        <div class="description-block">
-                            <h5 class="description-header">{{ $match->visitor_team_name }}</h5>
-                            <span class="description-text">{{ $match->visitor_score ?? 'Sem Resultado' }}</span>
-                        </div>
-
-                    </div>
-
-                    <div class="col-sm-12 border-top">
-                        <div class="description-block">
-                            <h5 class="description-header">Partida em</h5>
-                            <span class="description-text">{{ $match->schedule->format('d/m/Y H:i') }}</span>
-                            <div class="btn-group-vertical w-100 mt-1">
-                                <a href="{{ route('system.matches.form_update', [$team->id, $match->id]) }}" class="btn btn-lg w-100 btn-warning"> Editar Jogo</a>
-                                <a href="{{ route('system.match-players.form', [$team->id, $match->id]) }}" class="btn btn-lg w-100 btn-secondary"> Editar Jogadores</a>
-                                <a href="{{ route('system.matches.show', [$team->id, $match->id]) }}" class="btn btn-lg w-100 btn-primary"> Visualizar Jogo</a>
+            <div class="card card-widget widget-user">
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-sm-6 border-right">
+                            <div class="description-block">
+                                <h5 class="description-header"> {{ $match->home_team_name }} </h5>
+                                <span class="description-text"> {{ $match->home_score ?? 'Sem Resultado' }}</span>
                             </div>
-                         </div>
+
+                        </div>
+
+                        <div class="col-sm-6">
+                            <div class="description-block">
+                                <h5 class="description-header">{{ $match->visitor_team_name }}</h5>
+                                <span class="description-text">{{ $match->visitor_score ?? 'Sem Resultado' }}</span>
+                            </div>
+
+                        </div>
+
+                        <div class="col-sm-12 border-top">
+                            <div class="description-block">
+                                <h5 class="description-header">Partida em</h5>
+                                <span class="description-text">{{ $match->schedule->format('d/m/Y H:i') }}</span>
+                                <div class="btn-group-vertical w-100 mt-1">
+                                    <a href="{{ route('system.matches.form_update', [$team->id, $match->id]) }}" class="btn btn-lg w-100 btn-warning"> Editar Jogo</a>
+                                    <a href="{{ route('system.match-players.form', [$team->id, $match->id]) }}" class="btn btn-lg w-100 btn-secondary"> Editar Jogadores</a>
+                                    <a href="{{ route('system.matches.show', [$team->id, $match->id]) }}" class="btn btn-lg w-100 btn-primary"> Visualizar Jogo</a>
+                                </div>
+                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
+        @endforeach
 
-    </div>
-    @endforeach
-
-    @if($matches->links())
-    <div class="col-12 mt-3">
-        {{ $matches->withQueryString()->links() }}
-    </div>
-    @endif
+        @if($matches->links())
+            <div class="col-12 mt-3">
+                {{ $matches->withQueryString()->links() }}
+            </div>
+        @endif
     @endif
 </div>
 @endsection
