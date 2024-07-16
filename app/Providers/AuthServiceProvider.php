1a2b4d0e89f051d0fddefa190efe312c89c1592e
André Fontoura <anddfontoura91@gmail.com>
2024-07-15 21:22:24 -0300

    Traduz o e-mail de boas vindas do sistema

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +27,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->greeting(__('email.hello'))
+                ->subject(__('email.verify_email.verify_email_address'))
+                ->line(__('email.verify_email.please_click_button_to_verify'))
+                ->action(__('email.verify_email.verify_email_address'), $url)
+                ->line(__('email.verify_email.if_you_didnt_create_account'))
+                ->salutation(__('email.salutation'));
+        });
     }
 }
